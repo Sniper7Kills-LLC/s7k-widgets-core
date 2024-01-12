0517d696b351845899f52781de22f48ed18ba7af
Will G <sniper7kills@users.noreply.github.com>
2024-01-12 13:48:40 -0500

    styled tabs

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -5,13 +5,22 @@
     @change="changeTab"
     :key="layoutManager.currentLayout.tabs.length"
   >
-    <TabList class="isolate flex divide-x divide-gray-200 rounded-lg shadow">
+    <TabList class="isolate flex rounded-lg shadow">
       <Tab
-        class="text-gray-500 hover:text-gray-700 group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10"
+        as="template"
+        v-slot="{ selected }"
         v-for="tab in $props.tabs"
-        :key="tab.name"
+        :key="tab.id"
       >
-        {{ tab.name }}
+        <button
+          :class="{
+            'border-indigo-500 text-indigo-600': selected,
+            'text-gray-500 border-gray-200': !selected,
+            'w-1/4 border-b-2 py-4 px-1 text-center text-sm font-medium': true,
+          }"
+        >
+          {{ tab.name }}
+        </button>
       </Tab>
     </TabList>
     <TabPanels>
@@ -20,7 +29,7 @@
           :layout="tab.grid"
           :inEditMode="inEditMode"
           @layout-updated="(n) => gridUpdated(n)"
-        />
+        ></WidgetsGrid>
       </TabPanel>
     </TabPanels>
   </TabGroup>
@@ -65,7 +74,6 @@ export default defineComponent({
     );
 
     function changeTab(index: number) {
-      console.log(index);
       layoutManager.currentTab = index;
     }
 
